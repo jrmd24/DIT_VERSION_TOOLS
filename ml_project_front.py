018c9a263e4118c97f5b7076995acac5162f7db1
jrmd24 <jr.diouf@gmail.com>
2025-03-16 19:00:53 +0000

    Commit initial

---
+++ ml_project_front.py
@@ -0,0 +1,18 @@
+import base64
+
+import matplotlib.pyplot as plt
+import ml_project_back as mpb
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.markdown(
+    "<h1 style='text-align: center; color:#841230;'>Groupe 4 - Outils de Versioning 2025 </h1>",
+    unsafe_allow_html=True,
+)
+st.markdown(
+    "<h2 style='text-align: center; color:#841230;'>Gestion de données de Machine Learning</h2>",
+    unsafe_allow_html=True,
+)
